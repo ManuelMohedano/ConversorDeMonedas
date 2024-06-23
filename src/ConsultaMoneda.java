fdd26ddb87b282499025b1216c9a179a47088f75
J.Manuel <j.manuelmohedano@live.com.mx>
2024-06-23 17:13:45 -0600

    Adicion de archivos java y comenzamos con la funcionalidad de la conversion de monedas

---
+++ src/ConsultaMoneda.java
@@ -0,0 +1,29 @@
+import com.google.gson.Gson;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsultaMoneda {
+
+    ConsultaMoneda(String base,String target, double amount){
+        realizaCambio(base,target,amount);
+    }
+    private Moneda realizaCambio(String monedaBase, String monedaObjetivo, double cantidad){
+        String llave ="b1701122247ab8055158937a";
+        var url = "https://v6.exchangerate-api.com/v6/"+llave+"/pair/"+monedaBase+"/"+monedaObjetivo+"/"+cantidad;
+        URI direccion = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(direccion).build();
+
+        try{
+            HttpResponse<String> response = null;
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+            return new Gson().fromJson(response.body(), Moneda.class);
+        }catch (Exception e){
+            throw new RuntimeException("No pude realizar la conversión");
+        }
+    }
+}
--- src/Principal.java
+++ src/Principal.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class Principal {
+    public static void main(String[] args) {
+        Scanner lectura = new Scanner(System.in);
+
+//        ConsultaMoneda consulta = new ConsultaMoneda();
+//        Moneda moneda = consulta.realizaCambio("USD","MXN",100.0);
+//        System.out.println("Estas convirtiendo de "+moneda.base_code()+" a "+moneda.target_code());
+//        System.out.println(moneda.conversion_result());
+    }
 }
